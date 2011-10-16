689264d69b69b71a899a70793c2dadde9a168988
Stefan <astral.arbiter@gmail.com>
2011-10-16 17:30:05 +0300

    Initial Commit

---
+++ RPNCalc/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RPNCalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtlInput_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtlInput.Text == "Expression") txtlInput.Text = "";
+        }
+
+        private void txtAnswer_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtAnswer.Text == "Answer") txtAnswer.Text = "";
+        }
+    }
+}
